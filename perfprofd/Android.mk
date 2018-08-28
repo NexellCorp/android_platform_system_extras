bc22d04b44374438f2e146ca4797815ac94cbe5b
Sungwoo Park <swpark@nexell.co.kr>
2018-08-28 18:06:06 +0900

    nexell/quickboot: Disable perfprofd
    This patch disables perfprofd when quickbooting.
    
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- perfprofd/Android.mk
+++ perfprofd/Android.mk
@@ -67,7 +67,9 @@ LOCAL_MODULE := perfprofd
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
 LOCAL_SHARED_LIBRARIES += libcutils
+ifneq ($(QUICKBOOT), 1)
 LOCAL_INIT_RC := perfprofd.rc
+endif
 include $(BUILD_EXECUTABLE)
 
 # Clean temp vars
